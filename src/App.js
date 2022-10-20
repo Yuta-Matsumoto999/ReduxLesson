ce80c9167ea8d67203fc08215e688bbeebccde25
Yuta-Matsumoto999 <y.matsumoto0723@gmail.com>
2022-10-20 16:33:17 +0900

    カウントアップとカウントダウンを実装

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
+import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
+import { decrement, increment } from './redux/counterSlice';
 
 function App() {
+
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>count: { count }</h1>
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
     </div>
   );
 }
